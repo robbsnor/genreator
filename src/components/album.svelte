4c3e4f4d559c881f2210f7b7fe4f427e3c25ebb7
Robbin Schmidt <robbinschmidt@gmail.com>
2022-05-23 19:23:06 +0200

    feat: init:

---
+++ src/components/album.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title
+    export let image
+</script>
+
+<div class="album">
+    <div class="album__cover">{ image }</div>
+    <div class="album__title">{ title }</div>
+</div>
