c179ed949c7377b334cfab9d0f4511cd8e478346
Robbin Schmidt <robbinschmidt@gmail.com>
2022-05-25 19:26:09 +0200

    feat: spotify init

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,46 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
 	import Header from './../components/header.svelte';
 	import { Spotify } from './../service/spotify.service.ts';
     import Album from '../components/album.svelte';
-</script>
-<Header></Header>
 
-<Album title="yo"></Album>
-<Album title="yos"></Album>
+    var client_id = '4acb2c2beed54527aa5851b75467fe86';
+    var redirect_uri = 'http://localhost:3000/';
+
+    var scope = 'user-read-private user-read-email';
+
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+
+    function getHashValue(key) {
+        const matches = location.hash.match(new RegExp(key + '=([^&]*)'));
+        return matches ? matches[1] : null;
+    }
+
+    onMount(() => {
+        const accessToken = getHashValue('access_token');
+
+        fetch('https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+        })
+    });
+
+
+
+</script>
+
+<div>
+    <a href="{ url }">Login</a>
+</div>
 
 <style lang="scss" global>
     @import '../style/app';
--- src/service/spotify.service.ts
+++ src/service/spotify.service.ts
@@ -1,4 +1,2 @@
 export class Spotify {
-    public x = 'yoooo';
-
 }
