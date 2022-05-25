a0cb20625e6dcaae964ec6406466d0e6f4a3bb38
Robbin Schmidt <robbinschmidt@gmail.com>
2022-05-25 09:06:31 +0200

    feat: using sveltekit now

--- src/components/album.svelte
+++ src/components/album.svelte
@@ -1,9 +1,10 @@
-<script>
-    export let title
-    export let image
+<script lang="ts">
+    const title = '';
+    const cover = '';
 </script>
 
 <div class="album">
-    <div class="album__cover">{ image }</div>
     <div class="album__title">{ title }</div>
+    <div class="album__cover">{ cover }</div>
 </div>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Album from '../components/album.svelte';
+</script>
+
+<h1>Index</h1>
+
+<Album></Album>
+
+<style lang="scss">
+    h1{
+        color: $h1;
+    }
+</style>
